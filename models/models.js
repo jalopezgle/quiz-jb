09cae87cdf4403497e9282dfa054ee7ce980be3b
jalopezgle <jalopezgle@gmail.com>
2015-06-17 06:09:50 +0200

    Autoload

--- models/models.js
+++ models/models.js
@@ -37,10 +37,10 @@ exports.Quiz = Quiz;
 
 sequelize.sync().then(function (){
 	  Quiz.count().then(function (count){
-	  	 if (count===0){
+	  	 if (count===1){
 	  	 	Quiz.create({
-	  	 		pregunta: 'Capital de Italia',
-	  	 		respuesta: 'Roma'
+	  	 		pregunta: 'Capital de Singapur',
+	  	 		respuesta: 'Singapur'
 	  	 	});
 	  	Quiz.create({
 	  	 		pregunta: 'Capital de Portugal',
